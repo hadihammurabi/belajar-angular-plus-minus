59bb5bc64f7c5c17bb9dcf8126f650f8fb558ab6
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-29 08:23:12 +0700

    create about page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PagesModule } from './pages/pages.module';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  }
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from '../app.material.module';
 import { HomeComponent } from './home/home.component';
 import { ComponentsModule } from '../components/components.module';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
     AppMaterialModule,
